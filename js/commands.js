206c590cb1a77378e33d57f79145045eac9f9ac8
Azur1337 <azur99@web.dE>
2025-04-18 02:54:57 +0200

    dont need svelte, new start/basically done already lolol

---
+++ js/commands.js
@@ -0,0 +1,90 @@
+var github = "https://github.com/azurdev";
+var email = "mailto:azur@0x187.dev";
+
+aboutme = [
+  "<br>",
+  "<span class='underline'>Hi, I'm Azur! ᓚᘏᗢ</span>",
+  "<br>",
+  "<li>I’m currently studying as an Information Technology Assistant at [REDACTED].</li>",
+  "<li>My fields of interest are:</li>",
+  `<pre class="whitespace-pre-wrap">
+        - Frontend Development (SvelteKit>React, Tailwind CSS)
+        - Backend Development (Learning Rust)
+        - UI/UX Designing</pre>`,
+  `<li><pre class="whitespace-pre-wrap">I would love to collaborate with anyone who wants to build a professional website for their business or even a personal website to showcase their portfolio.
+   I’m passionate about learning new technologies and applying them to improve my skills.
+   I primarily use SvelteKit, and Tailwind CSS for frontend development and Rust for backend tasks.
+   You can learn more about the projects that I have built using the project command.
+   Browse around my website to find out more about me.
+   *Try using a social command!</pre></li>`,
+  `<li><pre class="whitespace-pre-wrap">Check out my GitHub profile to view my projects.
+   *Use the github command to take a look at my GitHub profile.</pre></li>`,
+  `<li>Contact me at <a href="mailto:azur@0x187.dev">azur@0x187.dev</a></li>`,
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'github         <a href="' +
+    github +
+    '" target="blank">github/azur1337' +
+    "</a>",
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  `<a href="https://ruhrpottmetaller.com" target="blank"><span class='underline'>Ruhrpottmetaller Corporate Website</span></a>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">A corporate website built for Ruhrpottmetaller.com to showcase services and enable customer communication.
+Styled with Tailwind CSS and powered by SvelteKit for a responsive and user-friendly experience.
+Backend functionality was implemented using Rust for handling emails and other server-side tasks.
+
+Project is live here <a href="https://ruhrpottmetaller.com" target="blank">https://ruhrpottmetaller.com</a>
+
+Built With:
+    *SvelteKit
+    *Tailwind CSS
+    *Rust
+`,
+
+  "<br>",
+
+  `<a href="/" target="blank"><span class='underline'>GFOS Innovationsaward Web Application</span></a>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">A web application designed to track student attendance in schools.
+The app features a sleek UI dashboard and detailed documentation.
+Awarded 1st place in the GFOS Innovationsaward for its innovative approach and technical execution.
+
+Built With:
+    *SvelteKit
+    *JavaScript
+    *Tailwind CSS
+`,
+
+  "<br>",
+];
+
+help = [
+  `<br><pre class="whitespace-pre-wrap">
+  <span class="command">aboutme</span>    
+   <span class="command">↳</span>&nbsp;Displays who I am.
+  <span class="command">social</span>     
+   <span class="command">↳</span>&nbsp;Lists social networks.
+  <span class="command">projects</span>   
+   <span class="command">↳</span>&nbsp;View coding projects.
+  <span class="command">email</span>      
+   <span class="command">↳</span>&nbsp;To send me an email.
+  <span class="command">history</span>    
+   <span class="command">↳</span>&nbsp;View command history.
+  <span class="command">help</span>       
+   <span class="command">↳</span>&nbsp;Displays this help message.
+  <span class="command">sudo</span>       
+   <span class="command">↳</span>&nbsp;Try it out for yourself.
+  <span class="command">clear</span>
+   <span class="command">↳</span>&nbsp;Clear terminal.</pre><br>`,
+];
+
+banner = [
+  `<div class="pt-2"><span class="text-[#C4A7E7] ">Welcome to my portfolio! — Type <span class="command text-[#75e1e7]">help</span> for a list of supported commands.</span></div>`,
+];
